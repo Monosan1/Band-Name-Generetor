3e1d5f5855a17bc0e1c96ddf13ff9f8cce2b0f67
Gabriel Araujo <50842610+Monosan1@users.noreply.github.com>
2021-09-01 04:16:40 +0000

    minor changes

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-name = input("Type your name: ")
-print("Hello  "+ name +"! Welcome to heaven \n To find your band name type the following data:")
+#This is where you started, first of all, say your name
+name = input("Whats your name? ")
+
+#Print to the user whats this program do
+
+print("Hello  "+ name +"! Welcome my first python project \n To find whats your band name type the following data:")
+
+#Get the 2 information that we need
+
 city = input("Type the city that you was born: ")
 pet = input("And now, type the name of your first pet: ")
 
-if len(city) > len(pet):
-  print (city + " " + pet)
-else:
-  print(pet + " " + city) 
+#And basead on the length (len()) of the answers, it switches the order
 
+if len(city) > len(pet):
+  print ("Your band name is: " + city + " " + pet)
+else:
+  print("Your band name is: " + pet + " " + city) 
+
+#Thats it, thanks for passing by xD
